1cef39394c24113edc1bcfa10c76fed03aae2395
Dyla n ra y <technicalpro@securemedia.ch>
2020-01-19 03:30:34 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,15 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
   def self.create 
-    song = Song.new
+   song = Song.new
+   song.save
+   return song
 end 
-
   def self.all
     @@all
+  end
+    def new_by_name
+    song = Song.new_by_name("Blank Space")
   end
 
   def save
