4e46ecea3aa7073d4da707b08309778129e14d0f
victor tim <technicalpro@securemedia.ch>
2020-01-31 03:46:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,4 @@
- Song
+ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
@@ -13,9 +13,38 @@ def self.all
     @@all
 end
   
-def self.new_by_name
-    song = Song.create_new_by_name("property")
+def self.new_by_name(property)
+    song = Song.create_new_by_name
     song.save
+    
+end
+
+def self.create_by_name
+  
+end
+
+def self.find_by_name
+  
+end
+
+def self.find_or_create_by_name
+
+end
+
+def self.alphabetical
+
+end
+
+def self.new_from_filename
+
+end
+
+def self.create_from_filename
+
+end
+
+def self.destroy_all
+
 end
 
 def save
